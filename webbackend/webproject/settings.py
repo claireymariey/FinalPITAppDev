023d20776aab495e4fd1539a318a682457fe9025
Claire <claire.mb0504@gmail.com>
2025-05-22 19:16:12 +0800

    Fix: Add STATIC_ROOT for collectstatic

--- webbackend/webproject/settings.py
+++ webbackend/webproject/settings.py
@@ -68,6 +68,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # must be first among common middlewares
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -145,6 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
